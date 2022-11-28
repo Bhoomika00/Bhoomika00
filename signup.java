8c24db16a138211f475480a2e66e753c1fdd73e3
Bhoomika00 <62229567+Bhoomika00@users.noreply.github.com>
2022-11-28 12:52:02 +0530

    Add files via upload

--- signup.java
+++ signup.java
@@ -40,5 +40,40 @@ public class signup {
 		
 			
 	}
+	
+	public static void forgot_password() {
+		
+		try (Connection conn=DriverManager.getConnection(DB_URL,DB_user,DB_pass);
+				 PreparedStatement ps=conn.prepareStatement("update register set password=? where email=? and password=? ");)
+			 {
+				
+			System.out.println("Enter registered email id");
+			String email=sc.nextLine();
+			
+			System.out.println("Enter old password");
+			String old_pass=sc.nextLine();
+			
+			System.out.println("Enter new password");
+			String new_pass=sc.nextLine();
+			
+				 
+				 
+               ps.setString(1, new_pass);
+				 
+				 ps.setString(2,email);
+				 
+				 ps.setString(3, old_pass);
+				 
+				 ps.executeUpdate();
+				 System.out.println("Password changed successfully");				 				 		 
+			 }
+			 catch(SQLException e) {
+				 e.printStackTrace();
+					 }
+		
+	}
+	/*public static void main(String[] args) {
+		forgot_password();
+	}*/
 
 }
